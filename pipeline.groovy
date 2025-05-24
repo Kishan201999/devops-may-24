f9ce923b1c10dcf143bcc0f96288b59d65b80978
Kishan201999 <160528739+Kishan201999@users.noreply.github.com>
2025-05-24 14:31:34 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,23 +1,19 @@
 pipeline{
 	Stages{
-		stage ("AWS_Login"){
+		stage ("Building Image"){
 			script{
-					
+		aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-password 111111111111.dkr.ecr.ap-south-1.amazonaws.com
+                docker tag --
+                docker push --	
 			}	
 				}
-		stage ("Creation of ECS"){	
+		stage ("Pushing to ECS"){	
 			Script {
-
+  
 			}	
 		
 		}
-
-		stage ("Image push to ECS"){
-			Script {
-			}
-		
-		}
-			
+	
 	
 	
 	}
